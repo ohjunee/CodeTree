39235bb311bb0d20adadb175bd4eedbaa1ba4a6e
Ohjun Kwon <49416429+ohjunee@users.noreply.github.com>
2025-03-23 23:39:30 +0900

    Title: 최대공약수 구하기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250323/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 250323/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -2,8 +2,12 @@ n, m = map(int, input().split())
 
 # Please write your code here.
 def get_maximum_dividen(n, m):
-    for i in range(1, min(n, m)+1, 1):
-        if n % i == 0 and m % i == 0:
-            res = i
-    return res
+    a = max(n, m)
+    b = min(n, m)
+    if a % b:
+        r = a % b
+        return get_maximum_dividen(b, r)
+    else:
+        return b
+
 print(get_maximum_dividen(n, m))
